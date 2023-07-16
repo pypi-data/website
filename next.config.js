efa819ef67f77ed464a7ce6e6f14881101407d67
Tom Forbes <tom@tomforb.es>
2023-07-16 22:31:19 +0100

    Prefix

--- next.config.js
+++ next.config.js
@@ -12,12 +12,12 @@
 //   },
 // })
 const basePath = process.env.BASE_PATH;
-const assetPath = process.env.ASSET_PATH;
+const assetPrefix = process.env.ASSET_PATH;
 
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   basePath,
-  assetPath,
+  assetPrefix,
   // experimental: {
   //   mdxRs: true,
   // },
