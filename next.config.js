1999da8e68f3b1462b006ec239e103ec0c5a2b7f
Tom Forbes <tom@tomforb.es>
2023-07-16 20:37:51 +0100

    mkdir -p?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
 const nextConfig = {
-    output: 'export',
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
