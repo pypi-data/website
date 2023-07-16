99771208f67ea8dd99221367b780c879182911c5
Tom Forbes <tom@tomforb.es>
2023-07-16 20:28:00 +0100

    Try this

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,23 @@ import packageList from '@/../public/data/pages.json';
 import Link from 'next/link'
 
 // @ts-ignore
-const packages: string[] = packageList.packages;
+const packages: string[] = packageList.packages.slice(0, 1000);
+// @ts-ignore
+const totalPackages: number = packageList.total_packages;
 
 export default function ProjectsList() {
   return (
     <>
+      <article className="prose lg:prose-xl mb-3">
+        <h1>Projects List</h1>
+        <p>
+          Due to time constraints this page is not done yet and only the first 1,000 projects are shown.
+          There are {totalPackages} projects in total.
+        </p>
+        <p>
+          To visit a project use a direct URL, e.g so: <Link href={`/projects/django/`}>/projects/django</Link>
+        </p>
+      </article>
       <div className="overflow-x-auto">
         <table className="table">
           <thead>
@@ -22,7 +34,7 @@ export default function ProjectsList() {
                 <td>{p}</td>
                 <td>
                   <button className="btn btn-info btn-xs">
-                    <Link href={`/projects/${p}/`}>Home</Link>
+                    <Link href={`/projects/${p}/`}>View</Link>
                   </button>
                 </td>
               </tr>
