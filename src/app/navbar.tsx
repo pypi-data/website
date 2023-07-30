fe417c005686ca762c113e53e997c46666f95cb8
Tom Forbes <tom@tomforb.es>
2023-07-30 01:51:14 +0100

    Fix pypi name

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <div className="navbar bg-base-300 mb-5">
-            <Link href="/" className="btn btn-ghost normal-case text-xl" passHref={true}>PyPi Data</Link>
+            <Link href="/" className="btn btn-ghost normal-case text-xl" passHref={true}>PyPI Data</Link>
             <Link href="/stats/" className="btn btn-ghost normal-case text-large" passHref={true}>Stats for nerds</Link>
             <Link href="/projects/" className="btn btn-ghost normal-case text-large" passHref={true}>Projects</Link>
             <Link href="/datasets/" className="btn btn-ghost normal-case text-large" passHref={true}>Datasets</Link>
