3fb6475ca0705deb9c4ff9b881759d6bc3dad179
Tom Forbes <tom@tomforb.es>
2023-09-02 22:02:28 +0100

    Fix colours

--- src/app/stats/colours.ts
+++ src/app/stats/colours.ts
@@ -1,7 +1,7 @@
 import chroma from "chroma-js";
 
 // chroma.scale('RdYlBu').domain(myValues, 7, 'quantiles');
-let colors = ["orange", "skyblue", "yellow", "purple", "pink"];
+let colors = ["orange", "skyblue", "red"];
 
 export function genColours(value: number): string[] {
   return chroma.scale(colors).colors(value);
