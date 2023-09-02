66e8ada378b27ce0b102414a526d256be133f2cf
Tom Forbes <tom@tomforb.es>
2023-09-02 22:36:16 +0100

    mkdir

--- public/download.sh
+++ public/download.sh
@@ -5,6 +5,8 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
+mkdir -p "$1"
+
 for url in $(curl https://raw.githubusercontent.com/pypi-data/data/main/links/repositories.txt); do
     git -C "$1" clone "$url" --depth=1 --no-checkout --branch=code
 done
