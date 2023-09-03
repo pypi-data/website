8902ddc80a1ad1a5f66b989a267d179d8fae6be7
Tom Forbes <tom@tomforb.es>
2023-09-03 11:37:49 +0100

    Improve datasets page

--- src/app/datasets/clickhouse.tsx
+++ src/app/datasets/clickhouse.tsx
@@ -2,11 +2,7 @@
 
 import { useState } from "react";
 
-export default function ClickhouseView({
-                                         queries
-                                       }: {
-  queries: { name: string; query: string }[];
-}) {
+export default function ClickhouseView({ queries }: { queries: { name: string; query: string }[] }) {
   const [index, setIndex] = useState(0);
   const query = queries[index].query;
   const url = `https://play.clickhouse.com/play?user=play#${query}`;
@@ -15,18 +11,22 @@ export default function ClickhouseView({
       <div className="grid grid-cols-1 lg:grid-cols-5 text-center pb-5">
         {queries.map(({ name }, i) => (
           <div key={`click-${i}`}>
-            <button className={"btn btn-sm btn-primary"} onClick={() => {setIndex(i)}}>{name}</button>
+            <button
+              className={"btn btn-sm btn-primary"}
+              onClick={() => {
+                setIndex(i);
+              }}
+            >
+              {name}
+            </button>
           </div>
         ))}
       </div>
-      <iframe
-        key={index}
-        width={"100%"}
-        height={"600px"}
-        src={url}
-      ></iframe>
+      <iframe key={index} width={"100%"} height={"600px"} src={url}></iframe>
       <div className={"text-center pt-5"}>
-        <a className={"btn btn-sm btn-primary"} href={url} target="_blank">Open in new tab</a>
+        <a className={"btn btn-sm btn-primary"} href={url} target="_blank">
+          Open in new tab
+        </a>
       </div>
     </>
   );
