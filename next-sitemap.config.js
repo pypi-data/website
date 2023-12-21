d1cffbf7d77d563ba01eb029b1ebf30dd898072e
Tom Forbes <tom@tomforb.es>
2023-12-21 16:01:29 +0000

    Add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,39 +4,17 @@ const zlib = require('zlib');
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://py-code.org',
-  sitemapSize: 1000,
   generateIndexSitemap: true,
   generateRobotsTxt: true,
-  // additionalPaths: async (config) => {
-  //   const result = []
-  //
-  //     // public/data/pages.json
-  //   const packageListData_raw = fs.readFileSync(process.argv[2], "utf8");
-  //   const packageListData = zlib.unzipSync(packageListData_raw).toString();
-  //   const packageList = JSON.parse(packageListData);
-  //
-  //   return packageList.packages.map((pkg) => {
-  //
-  //   });
-  //
-  //   // all possible values
-  //   result.push({
-  //     loc: '/additional-page-2',
-  //     changefreq: 'yearly',
-  //     priority: 0.7,
-  //     lastmod: new Date().toISOString(),
-  //
-  //     // acts only on '/additional-page-2'
-  //     alternateRefs: [
-  //       {
-  //         href: 'https://es.example.com',
-  //         hreflang: 'es',
-  //       },
-  //       {
-  //         href: 'https://fr.example.com',
-  //         hreflang: 'fr',
-  //       },
-  //     ],
-  //   })
-  // },
+  additionalPaths: async (config) => {
+    const response = await fetch("https://data.py-code.org/data/pages.json")
+    const packageList = await response.json();
+
+    return packageList.packages.map((pkg) => ({
+      loc: `https://py-code.org/projects/view?name=${pkg}`,
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 }
--- scripts/build-index.ts
+++ scripts/build-index.ts
@@ -2,18 +2,21 @@ import Fuse from "fuse.js";
 import * as fs from "fs";
 import * as zlib from "zlib";
 
-const packageListData_raw = fs.readFileSync(process.argv[2], "utf8");
-const packageListData = zlib.unzipSync(packageListData_raw).toString();
-const packageList = JSON.parse(packageListData);
+async function fetchData() {
+  const response = await fetch("https://data.py-code.org/data/pages.json");
+  const packageList = await response.json();
 
-// @ts-ignore
-const packages: string[] = packageList.packages; //.slice(0, 100_000);
-// const packages: string[] = packageList.packages;
+  // @ts-ignore
+  const packages: string[] = packageList.packages; //.slice(0, 100_000);
+  // const packages: string[] = packageList.packages;
 
-const index = {
-  json: Fuse.createIndex([], packages).toJSON(),
-  packages: packages,
-};
-console.log(process.argv[3], JSON.stringify(index).length);
-const encoded = zlib.deflateSync(JSON.stringify(index), { level: 9 });
-fs.writeFileSync(process.argv[3], encoded);
+  const index = {
+    json: Fuse.createIndex([], packages).toJSON(),
+    packages: packages,
+  };
+  console.log(process.argv[2], JSON.stringify(index).length);
+  const encoded = zlib.deflateSync(JSON.stringify(index), { level: 9 });
+  fs.writeFileSync(process.argv[2], encoded);
+}
+
+fetchData();
